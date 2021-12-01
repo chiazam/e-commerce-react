b1146e534825413eed453e6e873d4d3c75fdb6fc
Miracle Nwabueze <m.nwabueze@verifyme.ng>
2021-12-01 19:02:22 +0100

    feat: Implement top nav header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './pages/home/index'
+import { Routes, Route } from "react-router-dom";
+import { NavHeader } from './pages/shared-components/nav-header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component {
+  render() {
+    return <div className="flex flex-col font-lato">
+      <NavHeader />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
     </div>
-  );
+  }
 }
 
 export default App;
---
+++ src/pages/home/featured-products.js
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+export class FeaturedProduct extends Component{
+    render(){
+        return <section className="flex flex-col bg-white py-28 px-1/5 w-full">
+
+            <h1 className="text-indigo-900 font-semibold text-4xl font-josefin-sans self-center">Featured Products</h1>
+
+            <div className="flex mt-12 w-full">
+
+
+
+
+
+
+                <div className="flex flex-col w-64">
+
+                    <section className="bg-gray-100 h-56 w-full">
+
+                    </section>
+
+                    <section className="w-full">
+
+                    </section>
+
+                </div>
+
+
+
+
+
+            </div>
+
+        </section>
+    }
+}
